e91ac408748ac4f5448bd78baf394d96299abfa0
Yichen Mo <amomorning@gmail.com>
2023-05-05 16:55:28 +0800

    Init repo

---
+++ main.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import os, socket, logging, click
+from flask import Flask, request, url_for, send_from_directory, send_file
+
+app = Flask(__name__)
+
+html = '''
+    <!DOCTYPE html>
+    <title>Upload File</title>
+    <h1>File Upload</h1>
+    <form method=post enctype=multipart/form-data>
+         <input type=file name=file>
+         <input type=submit value=upload>
+    </form>
+    '''
+    
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_url = url_for('uploaded_file', filename=filename)
+            return html + get_file_list()
+    return html + get_file_list()
+
+def get_file_list():
+    ret = "<p>Here are the files in the download directory:</p>"
+    ret += "<ul>"
+    for file in os.listdir(app.config['UPLOAD_FOLDER']):
+        ret += f"<li><a href='/download/{file}'>{file}</a></li>"
+    ret += "</ul>"
+    return ret
+
+
+@click.command()
+@click.option('--dir', default=os.path.join(os.getcwd(), 'download'), help="Usage:python .\main.py --dir='D:\amomorning\Desktop\\tmp'")
+def main(dir):
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.ERROR)
+
+    app.config['UPLOAD_FOLDER'] = dir
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+    host = socket.gethostbyname(socket.gethostname())
+    port = 5000
+    print(f" * Running on http://{host}:{port}")
+    print(f" * Syncing folder {app.config['UPLOAD_FOLDER']}")
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == '__main__':
+    main()
